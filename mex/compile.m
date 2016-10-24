32ee6725dee311cc5e157b55a0cbab121b9aa519
Brian DeAngelis <b.d.deangelis@gmail.com>
2016-10-24 11:42:02 -0400

    Use  correct directory separator for Macs

--- mex/compile.m
+++ mex/compile.m
@@ -3,8 +3,8 @@
 % mex('-largeArrayDims','-lopencl',['-L' oclPath 'lib\x86_64'],['-I' oclPath 'include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp')
 
 %% Nvidia:
-cudaPath = getenv('CUDA_PATH');
-mex('-largeArrayDims','-lopencl',['-L' cudaPath '\lib\x64'],['-I' cudaPath '\include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp')
+% cudaPath = getenv('CUDA_PATH');
+% mex('-largeArrayDims','-lopencl',['-L' cudaPath '\lib\x64'],['-I' cudaPath '\include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp')
 
 %% Mac:
-% mex -largeArrayDims extract2ndOrderKernelGPU.cpp ..\extractKernels.cpp ..\divideNumTauBlocks.cpp ..\OpenCLErrorStrings.cpp LDFLAGS="\$LDFLAGS -framework OpenCL"
+mex -largeArrayDims extract2ndOrderKernelGPU.cpp ../extractKernels.cpp ../divideNumTauBlocks.cpp ../OpenCLErrorStrings.cpp LDFLAGS="\$LDFLAGS -framework OpenCL"
