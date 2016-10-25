8e97dd0849687c4202b8b1d354226991ccce4ce4
Omer Mano <omer.mano@yale.edu>
2016-10-25 12:56:15 -0400

    Statically link on windows when compiling mex

--- mex/compile.m
+++ mex/compile.m
@@ -1,10 +1,10 @@
 %% AMD:
 % oclPath = getenv('AMDAPPSDKROOT');
-% mex('-largeArrayDims','-lopencl',['-L' oclPath 'lib\x86_64'],['-I' oclPath 'include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp')
+% mex('-largeArrayDims','-lopencl',['-L' oclPath 'lib\x86_64'],['-I' oclPath 'include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp','COMPFLAGS="$COMPFLAGS /MT"')
 
 %% Nvidia:
-% cudaPath = getenv('CUDA_PATH');
-% mex('-largeArrayDims','-lopencl',['-L' cudaPath '\lib\x64'],['-I' cudaPath '\include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp')
+cudaPath = getenv('CUDA_PATH');
+mex('-largeArrayDims','-lopencl',['-L' cudaPath '\lib\x64'],['-I' cudaPath '\include'],'extract2ndOrderKernelGPU.cpp','..\extractKernels.cpp','..\divideNumTauBlocks.cpp','..\OpenCLErrorStrings.cpp','COMPFLAGS="$COMPFLAGS /MT"')
 
 %% Mac:
-mex -largeArrayDims extract2ndOrderKernelGPU.cpp ../extractKernels.cpp ../divideNumTauBlocks.cpp ../OpenCLErrorStrings.cpp LDFLAGS="\$LDFLAGS -framework OpenCL"
+% mex -largeArrayDims extract2ndOrderKernelGPU.cpp ../extractKernels.cpp ../divideNumTauBlocks.cpp ../OpenCLErrorStrings.cpp LDFLAGS="\$LDFLAGS -framework OpenCL"
